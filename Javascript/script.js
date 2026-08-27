423dc7682e38701c0bf03575dbef4ae719b213cf
AXIS717 <axis.official.717@gmail.com>
2026-08-27 15:27:49 +0200

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,7 +3,7 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const targetDate = new Date("September 10 2026 10:00:00").getTime();
+const targetDate = new Date("November 10 2026 12:00:00").getTime();
 
 function timer () {
     const currentDate = new Date().getTime();
